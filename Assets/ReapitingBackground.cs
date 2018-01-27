0cd1664109c8153c6de387e7a8d337eaeccf90f0
Unknown <oscar.utreras@usach.cl>
2018-01-27 19:18:07 -0300

    Add scrolling background

---
+++ Assets/ReapitingBackground.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReapitingBackground : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
