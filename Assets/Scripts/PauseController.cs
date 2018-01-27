4bf2ed68121d073a22d7b4d82c9e404c9a7563c6
Luis <luis.tapia.a@usach.cl>
2018-01-27 05:43:02 -0300

    Pause agregado

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour {
+    public Transform pauseCanvas;
+    public Transform menu;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+		
+	}
+
+    public void Pause()
+    {
+        if (pauseCanvas.gameObject.activeInHierarchy == false)
+        {
+            pauseCanvas.gameObject.SetActive(true);
+            menu.gameObject.SetActive(false);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            pauseCanvas.gameObject.SetActive(false);
+            menu.gameObject.SetActive(true);
+            Time.timeScale = 1;
+        }
+    }
+}
